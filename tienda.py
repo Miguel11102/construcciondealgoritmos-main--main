71cbf0616d886b22724cf4fb8480185aa60b3097
Miguel11102 <160886700+Miguel11102@users.noreply.github.com>
2024-03-21 12:45:49 -0500

    Create tienda.py

---
+++ tienda.py
@@ -0,0 +1,18 @@
+from producto import producto
+class tienda:
+
+    """-------------------------------------------------------
+    #Atributos
+    --------------------------------------------------------"""
+
+    __producto1 = None
+    __producto2 = None
+    __producto3 = None
+    __producto4 = None
+
+    """--------------------------------------------------------------
+    #metodos
+    --------------------------------------------------------------"""
+    def getProducto1(self):
+        return self.__producto1
+
