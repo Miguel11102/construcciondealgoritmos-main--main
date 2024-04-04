1188940e5bc81543162b7b3cd62eb4e3e959ceb5
Miguel11102 <160886700+Miguel11102@users.noreply.github.com>
2024-04-04 12:41:41 -0500

    Update producto.py

--- producto.py
+++ producto.py
@@ -25,13 +25,20 @@ class producto:
     __nombre = None
     __tipo = Enum("tipo",["PAPELERIA", "SUPERMERCADO", "FARMACIA"])
     __valorUnitario = 0.0
-    __cantidadBodegas = 0
+    __cantidadBodega = 0
     __cantidadMinima = 0
     __cantidadUnidadesVendidas = 0
 
     """-------------------------------------------------------------------
     # Metodos
     --------------------------------------------------------------------"""
+    def __init__ (self,tipo, pNombre, pValorUnitario, pCantidadBodega, pCantidadMinima, pCantidadUnidadesVendidas):
+        self.__tipo
+        self.__nombre
+        self.__valorUnitario
+        self.__cantidadBodega
+        self.__cantidadMinima
+        self.__cantidadUnidadesVendidas=0
 
     def getNombre(self):
         return self.__nombre
@@ -43,10 +50,55 @@ class producto:
         return self.__valorUnitario
     
     def getCantidadBodegas(self):
-        return self.__cantidadBodegas
+        return self.__cantidadBodega
     
     def getCantidadMinima(self):
         return self.__cantidadMinima
     
     def getCantidadUnidadesVendidas(self):
         return self.__cantidadUnidadesVendidas
+    
+    def setNombre(self,nombre):
+        self.__nombre = nombre
+    
+    def setTipo (self,tipo):
+        self.__tipo = tipo
+    
+    def setValorUnitario (self,valorUnitario):
+        self.__valorUnitario = valorUnitario
+
+    def setCantidadBodega (self,cantidadBodega):
+        self.__cantidadBodega = cantidadBodega
+
+    def setCantidadMinima (self,cantidadMinima):
+        self.__cantidadMinima = cantidadMinima
+
+    def setCantidadUnidadesVendidas (self,cantidadUnidadesVendidas):
+        self.cantidadUnidadesVendidas = cantidadUnidadesVendidas
+
+    def Vender(self, cantidad):
+        if cantidad > self.__cantidadBodega:
+            self.__cantidadUnidadesVendidas += cantidad
+            self.__cantidadBodega += cantidad
+        else:
+            print("Cantidad no dispoible")
+
+    def Hay_Suficiente(self, cantidad):
+        #forma 1
+        if cantidad <= self.__cantidadBodega:
+            return True
+        
+    def DarIVA(self):
+        if(self.__tipo=="PAPELERIA"):
+            return self.__IVA_PAPELERIA
+        elif(self.__tipo=="FARMACIA"):
+            return self.__IVA_FARMACIA
+        elif(self.__tipo=="SUPERMERCADO"):
+            return self.__IVA_SUPERMECADO
+        
+        else:
+            print("Categoria de producto no existe")
+
+    def SubirValorUnitario(self):
+        if self.__valorUnitario< 1000:
+            self.__valorUnitario +=self.__valorUnitario*0.01
