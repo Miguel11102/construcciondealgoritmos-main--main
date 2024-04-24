6d744d5ea5c33b2636e32e5bf841d067e5db54df
Miguel11102 <160886700+Miguel11102@users.noreply.github.com>
2024-04-24 12:28:20 -0500

    Update curso2.py

--- curso2.py
+++ curso2.py
@@ -4,14 +4,14 @@ class curso:
     #atributos 
     ------------------------------------------"""
 
-   curso__notas = [3, 5, 3, 2, 3.5, 1, 3.5, 4, 3.5, 5, 4.5, 4.2]
+   __notas = [3, 5, 3, 2, 3.5, 1, 3.5, 4, 3.5, 5, 4.5, 4.2]
 
    """--------------------------------------------
     #metodos
     --------------------------------------------"""
 
    def elementos_Totales(self):
-     curso_notas=len(self.curso__notas)
+     curso_notas=len(self.__notas)
      return curso_notas
     
    def promedio_notas(self):
@@ -19,7 +19,7 @@ class curso:
        indice = 0
 
        while indice <= 12:
-          suma= suma+self.__nota[indice]
+          suma= suma+self.__notas[indice]
           indice +=1
           
           return suma/indice
@@ -29,7 +29,7 @@ class curso:
        indice = 0
 
        while indice <12:
-          if (self.__notas[indice]>=3.0)and(self._notas[indice]<=5):
+          if (self.__notas[indice]>=3.0)and(self.__notas[indice]<=5):
              aprobados+=1
              indice+=1
 
@@ -39,7 +39,7 @@ class curso:
       aprobados = 0
 
       for indice in range(12):
-         if (self.__notas[indice]>=3.0)and(self._notas[indice]<=5):
+         if (self.__notas[indice]>=3.0)and(self.__notas[indice]<=5):
             aprobados+=1
    
       return aprobados
@@ -54,7 +54,7 @@ class curso:
    
    def calcularMayorNota(self):
       maxima=0
-      for nota in self.__nota:
+      for nota in self.__notas:
          if nota > maxima:
             maxima = nota
       return maxima
@@ -63,6 +63,57 @@ class curso:
       indice = 0
       while indice<12:
          if self.nota[indice]<=4.75:
-            self.notas[indice]*=1.05
+            self.__notas[indice]*=1.05
             indice*=1
-            return self.notas  
+            return self.__notas  
+
+   
+   def hayAlgunCinco_uno(self):
+      i=0
+      hayCinco = False
+      while i<len(self.__notas) and not hayCinco:
+         if self.__notas[i] == 5.0:
+            hayCinco = True
+         i+=1
+      return hayCinco
+
+   def hayAlgunCinco_dos(self):
+      hayCinco = False
+
+      for i in range(len(self.__notas)):
+         if (self.__notas[i] == 5):
+            hayCinco=True
+            break
+
+      return hayCinco
+   
+   def hayalgunCinco_tres(self):
+      hayCinco = False
+      for nota in self.__notas:
+         if nota == 5:
+            hayCinco=True
+            break
+      
+      return hayCinco
+   
+   def asignarNotas(self):
+      notasIguales = 0
+      for i in range (len(self.__notas)):
+         if self.__notas[i] == 1.5:
+            self.__notas[i] = 2.5
+         if notasIguales == 3:
+            break
+      return notasIguales
+   
+   def encontrarNotasCinco (self):
+      contador = 0
+      for i in range (len(self.__notas)):
+         if self.__notas[i] == 5.0:
+            contador += 1
+            contador == 3
+            return i
+         else:
+            return -1
+         
+
+
